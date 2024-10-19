98adc0e4f0cd434493d32b742a4536dcaa733ccb
Fatim-coding <timabadjie12@gmail.com>
2024-10-19 15:53:46 +0000

    second commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Assingning different variables
+name = "Pitbull"
+age = 11
+is-student = True
+weight = 27.8
+
+# printing Different variables and their Data type
+print("Name :",name)
+print("Data type of Name is", type(name))
+
+print("Age :", age)
+print("Data type of age is", type(age))
+
+print("is-student :", student)
+print("Data type of is-student", type(is stugdent))
+
+print("weight :", weight)
+print("Data type of weight is", type(weight))
+
+# type casting to convert the datatype of variable
+print("\n after type casting....")
+age = str(age)
+print("Data type of age is", type(age))
+weight = int(weight)
+print("Data type of weight is", type(weight))
